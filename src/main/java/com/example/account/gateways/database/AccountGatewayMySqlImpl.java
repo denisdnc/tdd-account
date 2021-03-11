129a50bd422fe2c2acacc0ea5a9be16287b2f245
Denis De Nadai Carraro <dcarraro@ciandt.com>
2021-03-11 18:46:55 -0300

    added update and find by id

--- src/main/java/com/example/account/gateways/database/AccountGatewayMySqlImpl.java
+++ src/main/java/com/example/account/gateways/database/AccountGatewayMySqlImpl.java
@@ -4,11 +4,14 @@ import com.example.account.entities.Account;
 import com.example.account.gateways.AccountGateway;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
 
 @Component
 public class AccountGatewayMySqlImpl implements AccountGateway {
 
-    JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
     public AccountGatewayMySqlImpl(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
@@ -18,16 +21,25 @@ public class AccountGatewayMySqlImpl implements AccountGateway {
     public void save(Account account) {
         jdbcTemplate.update(
                 "INSERT INTO accounts(id_conta, saldo) " +
-                    "VALUES (?, ?);", account.getId(), account.getBalance());
+                        "VALUES (?, ?);", account.getId(), account.getBalance());
     }
 
     @Override
     public void update(Account account) {
-
+        jdbcTemplate.update(
+                "UPDATE accounts SET saldo = ? WHERE id_conta = ? ", account.getBalance(), account.getId());
     }
 
     @Override
     public Account findById(String id) {
-        return null;
+        List<Account> query = jdbcTemplate.query(
+                "SELECT id_conta, saldo FROM accounts WHERE id_conta = ?",
+                (resultSet, i) -> new Account(resultSet.getString("id_conta"), resultSet.getDouble("saldo")), id);
+
+        if (CollectionUtils.isEmpty(query)) {
+            return null;
+        }
+
+        return query.get(0);
     }
 }
--- src/test/groovy/com/example/account/gateways/database/AccountGatewayMySqlImplTest.groovy
+++ src/test/groovy/com/example/account/gateways/database/AccountGatewayMySqlImplTest.groovy
@@ -6,12 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.ResultSetExtractor
+import org.springframework.jdbc.core.RowMapper
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.jdbc.Sql
 import org.springframework.test.context.jdbc.SqlGroup
 import org.springframework.test.context.jdbc.SqlMergeMode
 import org.springframework.transaction.annotation.Transactional
 import spock.lang.Specification
+
+import java.sql.ResultSet
 
 @SpringBootTest
 @AutoConfigureTestDatabase
@@ -48,6 +52,54 @@ class AccountGatewayMySqlImplTest extends Specification {
                         "WHERE id_conta = '%s';", account.id), Integer.class)
 
         result == 1
+    }
+
+    @Transactional
+    @Sql(scripts = "classpath:scripts/dml.sql")
+    def "deve buscar por id com sucesso"() {
+        when: "buscar por id"
+
+        Account account = accountGateway.findById("89ba0303-6a8b-40bc-b180-19300ea967c0")
+
+        then: "deve conter as seguintes propriedades"
+
+        account != null
+        account.balance == 1000d
+        account.id == "89ba0303-6a8b-40bc-b180-19300ea967c0"
+    }
+
+    @Transactional
+    def "deve buscar por id com resultado vazio"() {
+        when: "buscar por id"
+
+        Account account = accountGateway.findById("89ba0303-6a8b-40bc-b180-19300ea967c0")
+
+        then: "deve conter as seguintes propriedades"
+
+        account == null
+    }
+
+    @Transactional
+    @Sql(scripts = "classpath:scripts/dml.sql")
+    def "fazer update com sucesso"() {
+        given: "um conta"
+
+        Account account = new Account("89ba0303-6a8b-40bc-b180-19300ea967c0", 500d)
+
+        when: "atualizar por id"
+
+        accountGateway.update(account)
+
+        then: "o registro deve estar no banco de dados atualizado"
+
+        RowMapper<Account> extractor = { ResultSet resultSet, i -> new Account(resultSet.getString("id_conta"), resultSet.getDouble("saldo")) }
+
+        List<Account> updatedRecords = jdbcTemplate.query(
+                "SELECT id_conta, saldo FROM accounts WHERE id_conta = ?",
+                extractor, account.id);
+
+        updatedRecords.size() == 1
+        updatedRecords[0].balance == 500d
     }
 
 }
