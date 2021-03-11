53aacace995995fb9035b3e1eab901aaf8bb5c7d
Denis De Nadai Carraro <dcarraro@ciandt.com>
2021-03-11 11:48:59 -0300

    added spock and db

---
+++ src/test/groovy/account/AccountApplicationTest.groovy
@@ -0,0 +1,18 @@
+package account
+
+import com.example.account.AccountApplication
+import spock.lang.Specification
+
+class AccountApplicationTest extends Specification {
+
+    def "contextLoads"() {
+        when: "start spring boot application"
+
+        AccountApplication.main()
+
+        then: "should not throw error"
+
+        notThrown(Exception)
+    }
+
+}
